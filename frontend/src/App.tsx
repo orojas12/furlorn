9dda81aa928544dec2fb55668a637a5dc80ec62d
Oscar <orojas12@outlook.com>
2022-06-27 17:04:14 -0600

    add route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
       </Routes>
     </div>
   );
