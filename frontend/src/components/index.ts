94c63467f262b6580edbb1d892a0709998512eef
Oscar <orojas12@outlook.com>
2022-06-27 17:17:54 -0600

    add NavToggle and Button export

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 import Nav from "./Nav/Nav";
 import NavToggle from "./Nav/NavToggle";
 import Header from "./Header/Header";
+import Button from "./Button/Button";
 
-export { Nav, Header };
+export { Nav, NavToggle, Header, Button };
