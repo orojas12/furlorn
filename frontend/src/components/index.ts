2dddbf64bf0c1d9eb6e8356a12d969a2886913ca
Oscar <orojas12@outlook.com>
2022-06-29 15:05:11 -0600

    move Hero component into Home page

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,6 +2,4 @@ import Nav from "./Nav/Nav";
 import NavToggle from "./Nav/NavToggle";
 import Header from "./Header/Header";
 import Button from "./Button/Button";
-import Hero from "./Hero/Hero";
-
-export { Nav, NavToggle, Header, Button, Hero };
+export { Nav, NavToggle, Header, Button };
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-import { Header, Hero } from "../../components";
+import { Header, Button } from "../../components";
+
+import heroImg from "../../assets/hero-img.png";
+
+import "./Home.scss";
 
 export default function Home() {
   return (
     <article className="Home">
       <Header />
-      <Hero />
+      <section className="hero">
+        <div className="hero-container">
+          <img src={heroImg} alt="Puppy" className="hero-container__img" />
+          <div className="heading-container">
+            <h1 className="heading-container__heading">Find your lost pet</h1>
+            <h2 className="heading-container__subheading">
+              Use the power of your local community to recover your beloved
+              friend
+            </h2>
+            <Button type="button" btnStyle="primary" onClick={() => {}}>
+              Get Started
+            </Button>
+          </div>
+        </div>
+      </section>
     </article>
   );
 }
