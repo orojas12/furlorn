6cf7cfa524a4d5b923c8e145d287597721b406db
Oscar <orojas12@outlook.com>
2022-06-30 13:05:51 -0600

    add about section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-import { Header, Button } from "../../components";
+import { Header, Button, Card } from "../../components";
 
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/images/hero-img.png";
+import neighborhoodIcon from "../../assets/icons/neighborhood-icon.svg";
+import searchIcon from "../../assets/icons/search-icon.svg";
+import mapIcon from "../../assets/icons/map-icon.svg";
 
 import "./Home.scss";
 
@@ -10,7 +13,7 @@ export default function Home() {
   return (
     <article className="Home">
       <Header />
-      <section className="hero">
+      <section id="hero">
         <div className="hero-container">
           <img src={heroImg} alt="Puppy" className="hero-container__img" />
           <div className="heading-container">
@@ -23,6 +26,25 @@ export default function Home() {
               Get Started
             </Button>
           </div>
+        </div>
+      </section>
+      <section id="about">
+        <div className="about-container">
+          <Card
+            image={neighborhoodIcon}
+            caption="Neighborhood reporting notifies you when your pet is spotted or found in
+        your local area."
+          />
+          <Card
+            image={searchIcon}
+            imageStyles={{ height: "60px" }}
+            caption="Use an advanced search tool to quickly find results that match your pet’s description."
+          />
+          <Card
+            image={mapIcon}
+            imageStyles={{ height: "80px" }}
+            caption="View lost and found pets in your neighborhood on an interactive map."
+          />
         </div>
       </section>
     </article>
