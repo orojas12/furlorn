c8909679836a941bddf643b602522aa123d9fe0d
Oscar <orojas12@outlook.com>
2022-06-30 14:10:51 -0600

    add signup section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,6 +6,10 @@ import heroImg from "../../assets/images/hero-img.png";
 import neighborhoodIcon from "../../assets/icons/neighborhood-icon.svg";
 import searchIcon from "../../assets/icons/search-icon.svg";
 import mapIcon from "../../assets/icons/map-icon.svg";
+import pawIcon from "../../assets/icons/paw-icon.svg";
+import piechartIcon from "../../assets/icons/piechart-icon.svg";
+import percentIcon from "../../assets/icons/percent-icon.svg";
+import houseIcon from "../../assets/icons/house-icon.svg";
 
 import "./Home.scss";
 
@@ -45,6 +49,47 @@ export default function Home() {
             imageStyles={{ height: "80px" }}
             caption="View lost and found pets in your neighborhood on an interactive map."
           />
+        </div>
+      </section>
+      <section id="signup">
+        <div className="signup-container">
+          <div className="statement-container">
+            <figure className="statement">
+              <img src={pawIcon} alt="Paw icon" className="statement__img" />
+              <figcaption className="statement__caption">
+                There are 135 million dogs and cats in the United States.
+              </figcaption>
+            </figure>
+            <figure className="statement">
+              <img
+                src={piechartIcon}
+                alt="Paw icon"
+                className="statement__img"
+              />
+              <figcaption className="statement__caption">
+                1 in 3 pets goes missing in their lifetime.
+              </figcaption>
+            </figure>
+            <figure className="statement">
+              <img
+                src={percentIcon}
+                alt="Paw icon"
+                className="statement__img"
+              />
+              <figcaption className="statement__caption">
+                Only 6% of dog owners and 2% of cat owners find their lost pets
+                at a shelter.
+              </figcaption>
+            </figure>
+            <figure className="statement">
+              <img src={houseIcon} alt="Paw icon" className="statement__img" />
+              <figcaption className="statement__caption">
+                60% of lost pets are recovered by searching within the
+                neighborhood.
+              </figcaption>
+            </figure>
+          </div>
+          <div className="form-container"></div>
         </div>
       </section>
     </article>
