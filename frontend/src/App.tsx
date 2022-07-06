4da6efa480a6e1344d497f8a7cd815f13f31022f
Oscar <orojas12@outlook.com>
2022-07-05 18:11:38 -0600

    add code splitting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { Home } from "./pages";
+import { Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.scss";
+
+const Home = React.lazy(() => import("./pages/Home/Home"));
+const SignUp = React.lazy(() => import("./pages/SignUp/SignUp"));
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
+      <React.Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </React.Suspense>
     </div>
   );
 }
