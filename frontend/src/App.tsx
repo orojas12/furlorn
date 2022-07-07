c899ccfc264035a4a3e3bf86c8e183666c577e62
Oscar <orojas12@outlook.com>
2022-07-07 01:25:47 -0600

    add login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 
 const Home = React.lazy(() => import("./pages/Home/Home"));
 const SignUp = React.lazy(() => import("./pages/SignUp/SignUp"));
+const Login = React.lazy(() => import("./pages/Login/Login"));
 
 function App() {
   return (
     <div className="App">
       <React.Suspense fallback={<h1>Loading...</h1>}>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </React.Suspense>
     </div>
