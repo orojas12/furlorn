e7f4ce88af10f89dc90c843a65b40ef56c471c6e
Oscar <orojas12@outlook.com>
2022-07-11 12:02:05 -0600

    add navigation to login page after sign up

--- src/components/Form/SignUpForm.tsx
+++ src/components/Form/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../lib/auth";
 import { Form, Field } from "../";
 import "./SignUpForm.scss";
@@ -10,8 +10,10 @@ export interface ISignUpFormProps {
 
 export default function SignUpForm({ title = "Sign Up" }: ISignUpFormProps) {
   const auth = useAuth();
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
@@ -19,11 +21,12 @@ export default function SignUpForm({ title = "Sign Up" }: ISignUpFormProps) {
   const [formError, setFormError] = useState("");
 
   async function onSubmit() {
-    const res = await auth.login(username, password);
+    const res = await auth.signup(username, password, nickname);
     if (!res.ok) {
       setFormError(res.error);
       return;
     }
+    navigate("/login", { replace: true });
   }
 
   return (
