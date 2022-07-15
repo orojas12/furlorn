ac8a0188b912a57ff19cedef42f5532c1dd33089
Oscar <orojas12@outlook.com>
2022-07-15 17:21:22 -0600

    add field exports

---
+++ src/components/Form/Fields/index.ts
@@ -0,0 +1,6 @@
+import TextField from "./TextField";
+import NumberField from "./NumberField";
+import SelectField from "./SelectField";
+import TextAreaField from "./TextAreaField";
+
+export { TextField, NumberField, SelectField, TextAreaField };
