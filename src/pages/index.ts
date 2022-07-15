29247652250bcd527d3d87e63175a80c8947da06
Oscar <orojas12@outlook.com>
2022-07-15 10:48:31 -0600

    import Login and CreatePost

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,6 @@
 import Home from "./Home/Home";
 import SignUp from "./SignUp/SignUp";
+import Login from "./Login/Login";
+import CreatePost from "./CreatePost/CreatePost";
 
-export { Home, SignUp };
+export { Home, SignUp, Login, CreatePost };
