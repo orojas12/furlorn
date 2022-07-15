10715f9c3795d74f382f2682f13ab820c6dcd5cf
Oscar <orojas12@outlook.com>
2022-07-15 17:21:38 -0600

    add form to CreatePost page

--- src/pages/CreatePost/CreatePost.tsx
+++ src/pages/CreatePost/CreatePost.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar } from "../../components";
+import CreatePostForm from "../../components/Form/CreatePostForm";
 import useAuth from "../../lib/auth/useAuth";
 
 export interface ICreatePostProps {}
@@ -8,7 +9,7 @@ export default function CreatePost(props: ICreatePostProps) {
   const auth = useAuth();
   return (
     <article id="CreatePost">
-      <Navbar />
+      <CreatePostForm />
     </article>
   );
 }
