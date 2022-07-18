08adbe06f933afdfb54f1fbdaa05a24c6ace9725
Oscar <orojas12@outlook.com>
2022-07-18 11:17:25 -0600

    add new field imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,30 @@ import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
 import Button from "./Button/Button";
 import Card from "./Card/Card";
-import Form from "./Form/Form";
-import Field from "./Form/Field";
-import SignUpForm from "./Form/SignUpForm";
-import LoginForm from "./Form/LoginForm";
+import Form from "./forms/Form";
+import {
+  TextField,
+  NumberField,
+  SelectField,
+  TextAreaField,
+  PasswordField,
+} from "./forms/fields";
+import SignUpForm from "./forms/SignUpForm";
+import LoginForm from "./forms/LoginForm";
+import Modal from "./ui/Modal";
 
-export { Navbar, Header, Button, Card, Form, Field, SignUpForm, LoginForm };
+export {
+  Navbar,
+  Header,
+  Button,
+  Card,
+  Form,
+  TextField,
+  NumberField,
+  SelectField,
+  TextAreaField,
+  PasswordField,
+  SignUpForm,
+  LoginForm,
+  Modal,
+};
