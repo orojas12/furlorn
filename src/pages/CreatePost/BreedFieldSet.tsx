bc6dc327c8eaa4d6f4d1a063c38f4044cc32c62b
Oscar <orojas12@outlook.com>
2022-07-24 13:00:04 -0600

    pass fetched breeds into breed selector components

--- src/pages/CreatePost/BreedFieldSet.tsx
+++ src/pages/CreatePost/BreedFieldSet.tsx
@@ -2,53 +2,54 @@ import React, { useState } from "react";
 import MixBreedSelector from "./MixBreedSelector";
 import PureBreedSelector from "./PureBreedSelector";
 import { SelectField, Button, Modal } from "../../components";
-import IBreed from "./IBreed";
+import { IBreed, IFetchedBreeds } from "../../services/pet";
 
 export interface IBreedFieldSetProps {
+  species: string;
   breedType: string;
-  breeds: IBreed[];
-  onSetSingleBreed: (breed: IBreed) => void;
+  fetchedBreeds: IFetchedBreeds;
+  selectedBreeds: IBreed[];
+  onSetBreed: (breed: IBreed) => void;
   onAddBreed: (breed: IBreed) => void;
-  onRemoveBreed: (breed: IBreed) => void;
+  onRemoveBreed: (id: number) => void;
   onClearAllBreeds: () => void;
   onBreedTypeChange: (breedType: string) => void;
 }
 
-export default function BreedFieldSet({
-  breedType,
-  breeds,
-  onSetSingleBreed,
-  onAddBreed,
-  onRemoveBreed,
-  onClearAllBreeds,
-  onBreedTypeChange,
-}: IBreedFieldSetProps) {
+export default function BreedFieldSet(props: IBreedFieldSetProps) {
   const [showSelectBreedModal, setShowSelectBreedModal] = useState(false);
 
   function handleSelectBreedModalToggle() {
     setShowSelectBreedModal((prevState) => !prevState);
   }
 
-  function handleCheckboxChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const id = parseInt(e.target.id);
-    const value = e.target.value;
+  function handleCheckboxChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    breed: IBreed
+  ) {
     if (e.target.checked) {
-      onAddBreed({ id, value });
+      props.onAddBreed({
+        id: breed.id,
+        name: breed.name,
+        animal: breed.animal,
+      });
     } else {
-      onRemoveBreed({ id, value });
+      props.onRemoveBreed(breed.id);
     }
   }
 
-  function getBreedName(): React.ReactNode {
-    return `${breeds.reduce(
+  function getSelectedBreedsLabel(): React.ReactNode {
+    return `${props.selectedBreeds.reduce(
       (prevBreed, currentBreed, index, arr) =>
-        prevBreed + `${currentBreed.value}${index < arr.length - 1 ? "/" : ""}`,
+        prevBreed + `${currentBreed.name}${index < arr.length - 1 ? "/" : ""}`,
       ""
-    )} ${breedType === "mix" && breeds.length > 0 ? "Mix" : ""}`;
+    )} ${
+      props.breedType === "mix" && props.selectedBreeds.length > 0 ? "Mix" : ""
+    }`;
   }
 
   return (
-    <fieldset className="CreatePostForm__breed">
+    <fieldset className="CreatePostForm__breed Field">
       <legend>Breed</legend>
       <SelectField
         id="breedType"
@@ -58,13 +59,15 @@ export default function BreedFieldSet({
           { label: "Purebred", value: "purebred" },
           { label: "Mix", value: "mix" },
         ]}
-        onChange={(e) => onBreedTypeChange(e.target.value)}
-        value={breedType}
+        onChange={(e) => props.onBreedTypeChange(e.target.value)}
+        value={props.breedType}
         required
       />
       <div className="Field">
-        <div className="CreatePostForm__selected-breeds">{getBreedName()}</div>
-        {breedType !== "unknown" ? (
+        <div className="CreatePostForm__selected-breeds">
+          {getSelectedBreedsLabel()}
+        </div>
+        {props.breedType !== "unknown" ? (
           <Button
             type="button"
             btnStyle="secondary"
@@ -79,46 +82,20 @@ export default function BreedFieldSet({
           onClose={handleSelectBreedModalToggle}
           title="Select Breed"
         >
-          {breedType === "mix" ? (
+          {props.breedType === "mix" ? (
             <MixBreedSelector
-              breeds={[
-                { id: 1, value: "breed1" },
-                { id: 2, value: "breed2" },
-                { id: 3, value: "breed3" },
-                { id: 4, value: "breed4" },
-                { id: 5, value: "breed5" },
-                { id: 6, value: "breed6" },
-                { id: 7, value: "breed7" },
-                { id: 8, value: "breed8" },
-                { id: 9, value: "breed9" },
-                { id: 10, value: "breed10" },
-                { id: 11, value: "breed11" },
-                { id: 12, value: "breed12" },
-                { id: 13, value: "breed13" },
-              ]}
-              selectedBreeds={breeds}
+              species={props.species}
+              fetchedBreeds={props.fetchedBreeds}
+              selectedBreeds={props.selectedBreeds}
               onChange={handleCheckboxChange}
-              onClearAll={onClearAllBreeds}
+              onClearAll={props.onClearAllBreeds}
             />
           ) : (
             <PureBreedSelector
-              breeds={[
-                { id: 1, value: "breed1" },
-                { id: 2, value: "breed2" },
-                { id: 3, value: "breed3" },
-                { id: 4, value: "breed4" },
-                { id: 5, value: "breed5" },
-                { id: 6, value: "breed6" },
-                { id: 7, value: "breed7" },
-                { id: 8, value: "breed8" },
-                { id: 9, value: "breed9" },
-                { id: 10, value: "breed10" },
-                { id: 11, value: "breed11" },
-                { id: 12, value: "breed12" },
-                { id: 13, value: "breed13" },
-              ]}
-              selectedBreeds={breeds}
-              onChange={onSetSingleBreed}
+              species={props.species}
+              fetchedBreeds={props.fetchedBreeds}
+              selectedBreeds={props.selectedBreeds}
+              onChange={props.onSetBreed}
             />
           )}
 
