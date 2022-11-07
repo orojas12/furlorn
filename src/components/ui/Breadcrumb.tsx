cfa2095fe021a3a255f3b3282edbf3066844e39c
Oscar <orojas12@outlook.com>
2022-11-06 17:31:40 -0700

    add doc note

--- src/components/ui/Breadcrumb.tsx
+++ src/components/ui/Breadcrumb.tsx
@@ -3,6 +3,7 @@ export interface IBreadcrumbProps {}
 export default function Breadcrumb(props: IBreadcrumbProps) {
   return (
     <nav aria-label="Breadcrumb" className="breadcrumb">
+      {/* ARIA roles are used here to keep accessibility in Safari when using list-style: none in CSS */}
       <ul role="list">
         <li role="listitem">
           <a href="#">Home</a>
